805d7df456d23c403c036f87ac490eb1211a5115
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-04-24 11:49:28 -0300

    TO TEST: adds video elements. seek on slider update.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ const SEEK_URL =
 const minDate = new Date("2023-01-08T00:00:00-03:00");
 const maxDate = new Date("2023-01-08T23:59:59-03:00");
 
+const NUM_VIDS = 8;
+const PER_ROW = Math.floor(NUM_VIDS ** (9 / 16));
+
 function timestampToText(ts) {
   const mDate = new Date(0);
   mDate.setUTCSeconds(ts);
   return mDate.toString();
+}
+
+function timestampToPosition(ts, ts0, ts1, pos0, pos1) {
+  return ((ts - ts0) / (ts1 - ts0)) * (pos1 - pos0) + pos0;
 }
 
 function findFilename(ts) {
@@ -21,17 +28,13 @@ function findFilename(ts) {
   };
 }
 
-function tsToPos(ts, ts0, ts1, pos0, pos1) {
-  return ((ts - ts0)/(ts1 - ts0)) * (pos1 - pos0) + pos0;
-};
-
 function findPosition(ts) {
   return function (tsl) {
     let [ts0, pos0] = tsl[0];
     for (let i = 1; i < tsl.length; i++) {
       const [ts1, pos1] = tsl[i];
       if (ts >= ts0 && ts <= ts1) {
-        return tsToPos(ts, ts0, ts1, pos0, pos1);
+        return timestampToPosition(ts, ts0, ts1, pos0, pos1);
       } else {
         [ts0, pos0] = tsl[i];
       }
@@ -46,38 +49,83 @@ async function fetchData() {
 }
 
 document.addEventListener("DOMContentLoaded", async (_) => {
-  window.seekData = await fetchData();
+  const seekData = await fetchData();
 
-  const videosEl = document.getElementsByClassName("video-container")[0];
+  const videosEl = document.getElementById("video-container");
   const sliderEl = document.getElementById("timestamp-slider");
-  const timestampEl = document.getElementById("timestamp-value");
 
   sliderEl.setAttribute("min", `${minDate.getTime() / 1000}`);
   sliderEl.setAttribute("max", `${maxDate.getTime() / 1000}`);
   sliderEl.setAttribute("value", `${minDate.getTime() / 1000}`);
 
-  timestampEl.innerHTML = timestampToText(sliderEl.value);
+  function updateVideos(currentTimestamp) {
+    const timestampEl = document.getElementById("timestamp-slider-value");
+    timestampEl.innerHTML = timestampToText(currentTimestamp);
 
-  sliderEl.addEventListener("input", (ev) => {
-    const mts = ev.target.value;
-    timestampEl.innerHTML = timestampToText(mts);
-
-    const fileNames = Object.values(window.seekData.ranges).map(
-      findFilename(mts)
+    const fileNames = Object.values(seekData.ranges).map(
+      findFilename(currentTimestamp)
     );
 
-    const positions = fileNames.map((fileName) => {
+    const positions = fileNames.map((fileName, idx) => {
+      const mVid = document.getElementById(`video-${idx}`);
+      const mSrc = document.getElementById(`video-source-${idx}`);
+
+      if (mVid) {
+        mVid.pause();
+        mVid.removeAttribute("data-position");
+      }
+
       if (fileName == "") return -1;
-      const mPos = findPosition(mts)(window.seekData.seeks[fileName]);
-      // TODO:
-      // load fileName
-      // when loaded, seek
+
+      const mPos = findPosition(currentTimestamp)(seekData.seeks[fileName]);
+
+      if (mVid) {
+        mSrc.setAttribute("src", fileName);
+        mVid.setAttribute("data-position", mPos);
+        mVid.load();
+      }
+
       return mPos;
     });
-  });
 
-  const seekKeys = Object.keys(window.seekData);
-  videosEl.innerHTML = seekKeys;
-  // TODO:
-  // populate <video> tags
+    // console.log(fileNames);
+    // console.log(positions);
+  }
+
+  videosEl.innerHTML = "";
+  for (let i = 0; i < NUM_VIDS; i++) {
+    const mVid = document.createElement("video");
+    const mSrc = document.createElement("source");
+
+    mVid.id = `video-${i}`;
+    mVid.classList.add("video");
+    mVid.setAttribute("playsinline", "");
+    mVid.setAttribute("muted", "");
+    mVid.style.width = `${100 / PER_ROW}%`;
+
+    mVid.addEventListener("loadeddata", (ev) => {
+      console.log("loadeddata");
+      const vidEl = ev.target;
+      vidEl.currentTime = vidEl.getAttribute("data-position") || 0;
+    });
+
+    mVid.addEventListener("seeked", (ev) => {
+      console.log("seeked");
+      ev.target.play();
+      ev.target.pause();
+    });
+
+    mSrc.id = `video-source-${i}`;
+    mSrc.setAttribute("src", "");
+    mSrc.setAttribute("type", "video/mp4");
+
+    mVid.appendChild(mSrc);
+    videosEl.appendChild(mVid);
+  }
+
+  updateVideos(sliderEl.value);
+
+  sliderEl.addEventListener("input", (ev) => {
+    updateVideos(ev.target.value);
+  });
 });
