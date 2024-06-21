b02c48ba2a6f42ab5a5164e7427032f198210791
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-06-21 18:47:15 -0300

    update subdomains

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const OBJS_URL = "https://raw.githubusercontent.com/acervos-digitais/oito-um-utils/main/metadata/objects-1152/objects.json";
 const SEEK_URL = "https://raw.githubusercontent.com/acervos-digitais/oito-um-utils/main/metadata/0801-1152/seek.json";
 
-const VIDEOS_URL = "//outros.acervos.me/videos/0801-500";
-const IMAGES_URL = "//outros.acervos.me/images/0801-500";
+const VIDEOS_URL = "//digitais.acervos.me/videos/0801-500";
+const IMAGES_URL = "//digitais.acervos.me/images/0801-500";
 
 function getGridDims(numVideos) {
   const videoArea = (window.innerWidth * window.innerHeight) / numVideos;
