f0cc95dc8d5065976b44f96fb17b72c2d24fc9be
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-07-04 03:40:11 -0300

    about texts

--- js/strings.js
+++ js/strings.js
@@ -127,9 +127,15 @@ const OBJ2LABEL = {
 
 const ABOUTTEXT = {
   en: `
+  <p>TIME: display videos from all cameras at a specified time.</p>
+  <p>LOCATION: display all available videos from a specified camera.</p>
+  <p>OBJECTS: navigate videos by specifying objects of interest.</p>
   <p>About: <a href="https://www.acervosdigitais.fau.usp.br/" target="_blank">project site</a></p>
   `,
   pt: `
+  <p>HORÁRIO: mostra videos de todas as cameras no horário especificado.</p>
+  <p>CAMERA: mostra todos os videos disponíveis da camera escolhida.</p>
+  <p>OBJETOS: navegação de videos por objetos de interesse.</p>
   <p>Sobre: <a href="https://www.acervosdigitais.fau.usp.br/" target="_blank">site do projeto</a></p>
   `,
 };
