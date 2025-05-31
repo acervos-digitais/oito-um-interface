4a44850d4ce1fd28b001d83c7326a55d2490c093
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-05-31 10:26:45 -0400

    acervos.at.eu.org

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const OBJS_URL = "https://raw.githubusercontent.com/acervos-digitais/oito-um-utils/main/metadata/objects-1152/objects.json";
 const SEEK_URL = "https://raw.githubusercontent.com/acervos-digitais/oito-um-utils/main/metadata/0801-1152/seek.json";
 
-const VIDEOS_URL = "//digitais.acervos.me/videos/0801-500";
-const IMAGES_URL = "//media.acervos.me/images/0801-500";
+const VIDEOS_URL = "//digitais.acervos.at.eu.org/videos/0801-500";
+const IMAGES_URL = "//media.acervos.at.eu.org/images/0801-500";
 
 function lang() {
   let mlang = localStorage.getItem("0801lang");
